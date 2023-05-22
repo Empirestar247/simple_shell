2192bc84f5fd4195f975200623e2403d1e04fe57
EMMANUEL NDUKWE NMAJU <enmaju37@gmail.com>
2023-05-22 06:41:44 -0700

    updated code

--- free_list.c
+++ free_list.c
@@ -1,54 +1,19 @@
 #include "shell.h"
 
 /**
- * com_cmd - executes an input cmd
- * @ary: pointer to arr of inputs commands
- * @env: double pointer to envoiron var
- * @y: arg vector
- * @j: input from user
- * @new_j: input without \n
- * @cn: no. of input cmds
- * Return: -1 failure, 0 success
+ * free_list - to frees a linked list
+ * @th: the head of linked list
+ * Return: nothing
  */
-int com_cmd(char **ary, char **env, char **y, char *j, char *new_j, int cn)
+void free_list(path_t *th)
 {
-	pid_t our_pid;
-	char *cant;
-	int signal;
-	struct stat statuses;
+	path_t *incomingnod;
 
-	if (ary == NULL || *ary == NULL || y == NULL || *y == NULL)
-		return (-1);
-	if (env == NULL || *env == NULL)
-		return (-1);
-	if (inbt(ary, env, j, new_j, cn) == 0)
-		return (0);
-	our_pid = fork();
-	if (our_pid == -1)
+	while (th != NULL)
 	{
-		write(STDOUT_FILENO, "forking error", 13);
-		return (-1);
+		incomingnod = th->next;
+		free(th->dir);
+		free(th);
+		th = incomingnod;
 	}
-	if (our_pid == 0)
-	{
-		if (ary[0][0] == '/')
-		{
-			if (stat(ary[0], &statuses) == -1)
-				mistake_h(y, ary, cn, j, new_j);
-			if (access(ary[0], X_OK) == -1)
-				mistake_h(y, ary, cn, j, new_j);
-			execve(ary[0], ary, NULL);
-		}
-		else
-		{
-			cant = handpath(ary[0], env);
-			if (cant == NULL)
-				mistake_h(y, ary, cn, j, new_j);
-			else
-				execve(cant, ary, NULL);
-		}
-	}
-	else
-		wait(&signal);
-	return (0);
 }
